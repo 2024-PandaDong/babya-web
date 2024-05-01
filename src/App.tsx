042f3eef86c018e591eb353911d7e3511344f5bb
developersihyeon <139722653+developersihyeon@users.noreply.github.com>
2024-05-01 20:24:00 +0900

    ⚙️ :: 기본 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
 import React from 'react';
-import './App.css';
+import Router from "src/components/Router/Router";
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+  return <Router />;
 }
 
 export default App;

027b78f76cf45de1eb3a1c4dacfd569fbaa65096
developersihyeon <139722653+developersihyeon@users.noreply.github.com>
2024-06-04 19:28:46 +0900

    faet :: 배너관리 페이지 Router 연결 #6

--- src/components/Router/router.tsx
+++ src/components/Router/router.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import LoginPage from "src/pages/LoginPage";
+import BannerPage from "src/pages/BannerPage";
 
 const Router = () => {
     return (
         <Routes>
             <Route path="/login" element={<LoginPage />} />
+            <Route path="/banner" element={<BannerPage />} />
         </Routes>
     )
 }
---
+++ src/pages/BannerPage.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Banner from "src/components/Banner/index";
+
+const BannerPage = () => {
+    return <Banner />
+}
+
+export default BannerPage;

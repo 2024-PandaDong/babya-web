c4d12a7032bf45913b14e08e34f60d330443ec52
developersihyeon <139722653+developersihyeon@users.noreply.github.com>
2024-07-09 21:56:31 +0900

    feat :: 페이지 스텁 추가

--- src/components/Router/router.tsx
+++ src/components/Router/router.tsx
@@ -5,15 +5,21 @@ import BannerPage from "src/pages/Banner/page";
 import BannerWritePage from "src/pages/BannerWrite/page";
 import BannerModifyPage from "src/pages/BannerModify/page";
 import QuizPage from "src/pages/Quiz/page";
+import Prepare from "src/components/Common/Prepare";
 
 const Router = () => {
     return (
         <Routes>
             <Route path="/login" element={<LoginPage />} />
+            <Route path="/" element={<Prepare />} />
             <Route path="/banner" element={<BannerPage />} />
             <Route path="/banner-write" element={<BannerWritePage />} />
             <Route path="/banner-modify/:id" element={<BannerModifyPage />} />
             <Route path="/quiz" element={<QuizPage />} />
+            <Route path="/company" element={<Prepare />} />
+
+            <Route path="/FAQ" element={<Prepare />} />
+            <Route path="/inquiry" element={<Prepare />} />
         </Routes>
     )
 }

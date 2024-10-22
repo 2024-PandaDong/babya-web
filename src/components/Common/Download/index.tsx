95aa1f6382e521131c9e6aea13f9f46aef651fdb
developersihyeon <139722653+developersihyeon@users.noreply.github.com>
2024-10-22 15:15:24 +0900

    feat :: 애기야 Android, ios 다운로드 생성 #36

--- src/components/Common/Download/index.tsx
+++ src/components/Common/Download/index.tsx
@@ -3,13 +3,21 @@ import * as S from "./style"
 import {Link} from "react-router-dom";
 
 const Download = () => {
+    const handleDownload = () => {
+        const fileUrl = '/app-release.apk';
+        const link = document.createElement('a');
+        link.href = fileUrl;
+        link.download = '애기야.apk';
+        link.click();
+    }
+
     return (
             <S.Container>
                 <S.Wrap>
-                    <Link to="">
+                    <Link to="#" onClick={handleDownload}>
                         <S.Text>Android Download</S.Text>
                     </Link>
-                    <Link to="">
+                    <Link to="https://apps.apple.com/kr/app/애기야/id6736818972" target="_blank">
                         <S.Text>iOS Download</S.Text>
                     </Link>
                 </S.Wrap>

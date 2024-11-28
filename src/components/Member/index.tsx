8f3023c474bfec534061335485b95b2497346e43
박규민 <131234408+kyumin7487@users.noreply.github.com>
2024-11-28 20:44:59 +0900

    fix :: mixins CSS 최적화 및 usememo 최적화 #38

--- src/components/Member/index.tsx
+++ src/components/Member/index.tsx
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useMemo } from "react";
 import * as S from "./style";
 import useMember from "src/hooks/Member/useMember";
 import SearchIcon from "src/assets/img/Common/SearchIcon.svg";
+import { MemberListProps } from "src/types/Member/Member.interface";
+
+interface MemberListItemProps {
+    item: MemberListProps;
+    handleClickPatch: (email: string, currentState: string) => Promise<void>;
+    formatDate: (dateString: string) => string;
+}
+
+const MemberListItem: React.FC<MemberListItemProps> = React.memo(({
+         item,
+         handleClickPatch,
+         formatDate
+}) => {
+    return (
+        <S.MemberList key={item.email}>
+            <S.NameWrap>{item.nickname}</S.NameWrap>
+            <S.EmailWrap>
+                <S.Email>{item.email}</S.Email>
+            </S.EmailWrap>
+            <S.StateWrap>{item.state}</S.StateWrap>
+            <S.CreateDateWrap>{formatDate(item.createdAt)}</S.CreateDateWrap>
+            <S.ManagementWrap>
+                <S.BtnWrap>
+                    <S.PatchBtn onClick={() => handleClickPatch(item.email, item.state)}>
+                        상태변경
+                    </S.PatchBtn>
+                </S.BtnWrap>
+            </S.ManagementWrap>
+        </S.MemberList>
+    );
+});
 
 const Member = () => {
     const {
@@ -13,13 +44,19 @@ const Member = () => {
         handleClickPatch,
     } = useMember();
 
-    const formatDate = (dateString: string) => {
-        const date = new Date(dateString);
-        const year = date.getFullYear();
-        const month = String(date.getMonth() + 1).padStart(2, '0');
-        const day = String(date.getDate()).padStart(2, '0');
-        return `${year}년${month}월${day}일`;
-    };
+    const formatDate = useMemo(() => {
+        return (dateString: string) => {
+            const date = new Date(dateString);
+            const year = date.getFullYear();
+            const month = String(date.getMonth() + 1).padStart(2, '0');
+            const day = String(date.getDate()).padStart(2, '0');
+            return `${year}년${month}월${day}일`;
+        };
+    }, []);
+
+    const displayList = useMemo(() => {
+        return (memberListFilter.length ? memberListFilter : memberList);
+    }, [memberListFilter, memberList]);
 
     return (
         <S.MemberBackground>
@@ -31,12 +68,16 @@ const Member = () => {
                     <S.Member>
                         <S.OptionWrap>
                             <S.MemberSearchWrap>
-                                <S.MemberSearchImg src={SearchIcon}></S.MemberSearchImg>
+                                <S.MemberSearchImg
+                                    src={SearchIcon}
+                                    loading="lazy"
+                                />
                                 <S.MemberSearch
                                     value={searchValue}
                                     placeholder="원하는 회원정보를 입력해주세요"
                                     onChange={handleChangeValue}
-                                    onKeyDown={handleKeydown}></S.MemberSearch>
+                                    onKeyDown={handleKeydown}
+                                />
                             </S.MemberSearchWrap>
                         </S.OptionWrap>
                         <S.MemberListWrap>
@@ -50,28 +91,15 @@ const Member = () => {
                                 <S.ManagementWrap>관리</S.ManagementWrap>
                             </S.MemberListTitle>
                             <S.MemberListBox>
-                                {(memberListFilter.length
-                                    ? memberListFilter
-                                    : memberList).map((item) => (
-                                    <S.MemberList key={item.email}>
-                                        <S.NameWrap>{item.nickname}</S.NameWrap>
-                                        <S.EmailWrap>
-                                            <S.Email>{item.email}</S.Email>
-                                        </S.EmailWrap>
-                                        <S.StateWrap>
-                                            {item.state}
-                                        </S.StateWrap>
-                                        <S.CreateDateWrap>{formatDate(item.createdAt)}</S.CreateDateWrap>
-                                        <S.ManagementWrap>
-                                            <S.BtnWrap>
-                                                <S.PatchBtn onClick={() => handleClickPatch(item.email, item.state)}>
-                                                    상태변경
-                                                </S.PatchBtn>
-                                            </S.BtnWrap>
-                                        </S.ManagementWrap>
-                                    </S.MemberList>
+                                {displayList.map((item) => (
+                                    <MemberListItem
+                                        key={item.email}
+                                        item={item}
+                                        handleClickPatch={handleClickPatch}
+                                        formatDate={formatDate}
+                                    />
                                 ))}
-                                <div id="observer" style={{minHeight: "10px"}}></div>
+                                <div id="observer" style={{ minHeight: "10px" }}></div>
                             </S.MemberListBox>
                         </S.MemberListWrap>
                     </S.Member>
